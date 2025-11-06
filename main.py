61761bcee0220b59c7fa9c8cb59a0ac05958bc5a
ayanabbas <ayanabbas@kali.ayan>
2025-11-07 00:27:41 +0530

    made screen manager

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from screens.HomeScreen import HomeScreen
-#from screens.add_ritual import AddRitualScreen
-
 
 class ClarityForge(MDApp):
     def build(self):
@@ -12,7 +10,6 @@ class ClarityForge(MDApp):
         
         sm=MDScreenManager()
         sm.add_widget(HomeScreen(name='home'))
-        #sm.add_widget(AddRitualScreen(name='add_ritual'))
         sm.current='home'
         return sm
     
